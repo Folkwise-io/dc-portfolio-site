7aa73b3c4b6e0c35679684e9654f08a9cc6c3f4b
Rahul Jain <rahulj93@gmail.com>
2021-04-05 16:58:51 -0400

    updated index file for express port

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -3,6 +3,22 @@ const app = express();
 
 // console.log("Hello World!!!!");
 
-app.listen(3000, () => {
-  console.log("server is listening on port 3000")
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', require('./api'))
+
+
+
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal Server Error')
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`)
 })
