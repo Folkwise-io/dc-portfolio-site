7b9b905f79c1bbf2ccea79dd6c564bd03a851cda
Rahul Jain <rahulj93@gmail.com>
2021-04-05 22:46:05 -0400

    added echo endpoint for express

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -9,7 +9,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', require('./api'))
-
+// app.get('./', function(req,res) {
+//   res.send('hello world') 
+// });
 
 
 app.use((err, req, res, next) => {
