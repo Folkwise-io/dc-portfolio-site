952287c4cc71552a17834c44b134e24ad5445622
Chris DiPiero <frogmaster99@msn.com>
2021-04-12 12:50:28 -0500

    added webpack config file and formatted

---
+++ packages/frontend/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path")
+
+module.exports = {
+  entry: "/src/index.js",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "./",
+  },
+  module: {
+    rules: [
+      test: /\.jsx?$/,
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/env", "@babel/react"],
+        plugins: [],
+    ],
+  },
+}
