5cd0c3db13645a9d1dfb5d99ce06e1288959bc42
Chris DiPiero <frogmaster99@msn.com>
2021-04-13 14:44:46 -0500

    index.js to tsx in pkg and wp.config

--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-  entry: "/src/index.js",
+  entry: "/src/index.tsx",
   target: "web",                                          //after ts
   mode: "development",                                    //after ts
   resolve: {
