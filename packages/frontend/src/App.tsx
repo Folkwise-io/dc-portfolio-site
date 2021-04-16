424ead99c89d37aa5c42f7f25d65b6031482d43b
Khuong <lekhuongy@gmail.com>
2021-04-15 22:49:17 -0500

    made componets and some styling

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,13 +1,52 @@
-import { hot } from "react-hot-loader"
-import * as React from "react"
-import "./style.css"
+import { hot } from 'react-hot-loader';
+import React, { useState } from 'react';
+import './style.css';
 
 const App = () => {
-  return(
-    <div className="title-box">
-      <h1 className="title">Ahoy there, Collectivists! Here be Dragons!! Yargh!</h1>
-    </div>
-  )
-}
+  const [counter, setCounter] = useState(0);
 
-export default hot(module)(App)
+  function changeCounter(action: string) {
+    switch (action) {
+      case 'INCREMENT':
+        setCounter(counter + 1);
+        break;
+      case 'DECREMENT':
+        setCounter(counter - 1);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return (
+    <main>
+      <section className="section-counter">
+        <div className="counter__text-box">
+          <div className="u-text-center u-margin-bottom-small">
+            <h1 className="heading-primary">Simple Counter App</h1>
+          </div>
+
+          <div className="counter__value">{counter}</div>
+
+          <button
+            className="counter__button btn--red"
+            onClick={() => changeCounter('DECREMENT')}
+          >
+            Decrement
+          </button>
+    
+          <button
+            className="counter__button btn--green"
+            onClick={() => changeCounter('INCREMENT')}
+          >
+            Increment
+          </button>
+
+          
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default hot(module)(App);
