76aa0a505f59dfbc274a415febaf4479a9c15ca3
Chris DiPiero <frogmaster99@msn.com>
2021-04-16 09:55:18 -0500

    modified jes.config

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  setupFilesAfterEnv: [
+    "@testing-library/react/cleanup-after-each",
+    "@testing-library/jest-dom/extend-expect"
+  ],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
