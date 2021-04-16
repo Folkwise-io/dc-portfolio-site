484938c9e31c19132f2d3d94cacbe2ec8796c959
Khuong <lekhuongy@gmail.com>
2021-04-16 00:15:53 -0500

    grabs counter from db and increment and decrement changes db values

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,22 +3,33 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 import axios from 'axios';
 
+interface Counter {
+  counter: number;
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  useEffect( () =>{
-    const data = (async () => {
-      return await axios.get('http://localhost:5000/counter')
-  })();
-    console.log(data)
-  })
+  useEffect(() => {
+    (async () => {
+      await axios.get<Counter>('http://localhost:5000/counter').then(response => {
+        setCounter(response.data.counter);
+    });
+    })()
+  },[]);
 
   function changeCounter(action: string) {
     switch (action) {
       case 'INCREMENT':
+        (async () => {
+          await axios.post<Counter>('http://localhost:5000/counter', {"change" : "INCREMENT"})
+        })()
         setCounter(counter + 1);
         break;
       case 'DECREMENT':
+        (async () => {
+          await axios.post<Counter>('http://localhost:5000/counter', {"change" : "DECREMENT"})
+        })()
         setCounter(counter - 1);
         break;
       default:
@@ -42,15 +53,13 @@ const App = () => {
           >
             Decrement
           </button>
-    
+
           <button
             className="counter__button btn--green"
             onClick={() => changeCounter('INCREMENT')}
           >
             Increment
           </button>
-
-          
         </div>
       </section>
     </main>
