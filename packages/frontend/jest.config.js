131cc3056d605df83d8f5d20aee0e5ced0040373
Chris DiPiero <frogmaster99@msn.com>
2021-04-16 12:24:30 -0500

    fixed, removed cleanup (depr)

--- packages/frontend/jest.config.js
+++ packages/frontend/jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   setupFilesAfterEnv: [
-    "@testing-library/react/cleanup-after-each",
     "@testing-library/jest-dom/extend-expect"
   ],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
