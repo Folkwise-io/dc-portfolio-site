3969f76888580661787b40a605b03b84d7bfb6e6
Connie Tran <connietrandev@gmail.com>
2021-04-18 17:10:07 -0400

    installed Sass packages for React frontend

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,12 @@
+// Packages
 import { hot } from "react-hot-loader"; 
 import * as React from "react"; 
-import "./style.css"; 
 import { HashRouter, Route, Switch } from 'react-router-dom'; 
 
+// Styles
+import "./style.scss"; 
+
+// Components
 import Home from './components/home'; 
 import Counter from './components/counter'; 
 
