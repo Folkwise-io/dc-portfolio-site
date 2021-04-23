d3e789bc32da8281c7fb98332738bd629be24573
Khuong <lekhuongy@gmail.com>
2021-04-23 15:15:29 -0500

    added typing for window

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,21 +1,28 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom";
-import App from "./App";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
 import { createStore } from 'redux';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
 
-import { increment, decrement } from './actions';  
+import { increment, decrement } from './actions';
 
-const store = createStore(rootReducer);  
+const store = createStore(rootReducer);
+declare global {
+  interface Window {
+    store: any;
+    increment: any;
+    decrement: any;
+  }
+}
 
-window.store = store; 
-window.increment = increment; 
-window.decrement = decrement; 
+window.store = store;
+window.increment = increment;
+window.decrement = decrement;
 
 ReactDOM.render(
- <Provider store={store}>
+  <Provider store={store}>
     <App />
- </Provider>, 
- document.getElementById("app")
-)
+  </Provider>,
+  document.getElementById('app')
+);
