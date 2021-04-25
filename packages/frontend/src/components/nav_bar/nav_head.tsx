881a0b98bbb9124a780d63ddc9d884dca5192d33
Chris DiPiero <frogmaster99@msn.com>
2021-04-25 10:39:52 -0500

    added nav+bar component file and set text

---
+++ packages/frontend/src/components/nav_bar/nav_head.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+const NavBar = () => {
+  return (
+    <main>
+      <nav>
+        <ul>
+          <li>Simulation</li>
+          <li>Explore</li>
+          <li>Learn</li>
+          <li>Sign in</li>
+          <li>Sign up</li>
+        </ul>
+      </nav>
+    </main>
+  )
+}
+
+export default NavBar;
